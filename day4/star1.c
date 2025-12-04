838f91830af7462de1a65575d6594ba4fc2b504c
Rutger Wuijster <rutgerwuijster@hotmail.com>
2025-12-04 15:10:03 +0100

    Add comments cuz I need to know wth I was doing in 2 weeks

--- day4/star1.c
+++ day4/star1.c
@@ -15,24 +15,25 @@ int** readGrid() {
     for (int y = 0; y < GRID_SIZE; y++) {
         grid[y] = calloc(GRID_SIZE, sizeof(int));
         for (int x = 0; x < GRID_SIZE; x++) {
-            grid[y][x] = (getchar() == '@');
+            grid[y][x] = (getchar() == '@'); // Set grid position to 1 if paper roll is present
         }
-        getchar();
+        getchar(); // Remove newline character from stdin
     }
     return grid;
 }
 
 void printGrid(int** grid, int gridSize) {
     for (int y = 0; y < gridSize; y++) {
-        printf("%d", grid[y][0]);
+        printf("%d", grid[y][0]); // Print first character of current row
         for (int x = 1; x < gridSize; x++) {
-            printf(" %d", grid[y][x]);
+            printf(" %d", grid[y][x]); // Print aditional characters of row
         }
-        putchar('\n');
+        putchar('\n'); // Add newline to finish row
     }
 }
 
 int get(int** grid, int x, int y) {
+    // get Is a function that returns 0 when trying to access out of bounds
     if (x < 0) return 0;
     if (y < 0) return 0;
     if (x >= GRID_SIZE) return 0;
@@ -44,15 +45,15 @@ int countAllNeighbors(int** grid) {
     int total = 0;
     for (int y = 0; y < GRID_SIZE; y++) {
         for (int x = 0; x < GRID_SIZE; x++) {
-            if (grid[y][x] == 0) continue;
+            if (grid[y][x] == 0) continue; // If there is no paper roll it is not accessible
             int neighbors = 0;
             for (int i = -1; i < 2; i++) {
                 for (int j = -1; j < 2; j++) {
-                    if (i == j & j == 0) continue;
-                    neighbors += get(grid, x+i, y+j);
+                    if (i == j & j == 0) continue; // Center of looking is the roll itself, you are not your own neighbor
+                    neighbors += get(grid, x+i, y+j); // Add all rolls to the neighbors count
                 }
             }
-            if (neighbors < 4) total++;
+            if (neighbors < 4) total++; // If less than 4 neighboring rolls increase total count
         }
     }
     return total;
@@ -61,6 +62,6 @@ int countAllNeighbors(int** grid) {
 int main() {
     int** grid = readGrid();
     int amount = countAllNeighbors(grid);
-    freeMatrix(grid, GRID_SIZE);
+    freeMatrix(grid, GRID_SIZE); // Always clean up after yourself
     printf("Total accessible rols of paper: %d\n", amount);
 }
--- day4/star2.c
+++ day4/star2.c
@@ -15,24 +15,25 @@ int** readGrid() {
     for (int y = 0; y < GRID_SIZE; y++) {
         grid[y] = calloc(GRID_SIZE, sizeof(int));
         for (int x = 0; x < GRID_SIZE; x++) {
-            grid[y][x] = (getchar() == '@');
+            grid[y][x] = (getchar() == '@'); // Set grid position to 1 if paper roll is present
         }
-        getchar();
+        getchar(); // Remove newline character from stdin
     }
     return grid;
 }
 
 void printGrid(int** grid, int gridSize) {
     for (int y = 0; y < gridSize; y++) {
-        printf("%d", grid[y][0]);
+        printf("%d", grid[y][0]); // Print first character of current row
         for (int x = 1; x < gridSize; x++) {
-            printf(" %d", grid[y][x]);
+            printf(" %d", grid[y][x]); // Print aditional characters of row
         }
-        putchar('\n');
+        putchar('\n'); // Add newline to finish row
     }
 }
 
 int get(int** grid, int x, int y) {
+    // get Is a function that returns 0 when trying to access out of bounds
     if (x < 0) return 0;
     if (y < 0) return 0;
     if (x >= GRID_SIZE) return 0;
@@ -44,17 +45,17 @@ int countAllNeighbors(int** grid) {
     int total = 0;
     for (int y = 0; y < GRID_SIZE; y++) {
         for (int x = 0; x < GRID_SIZE; x++) {
-            if (grid[y][x] == 0) continue;
+            if (grid[y][x] == 0) continue; // If there is no paper roll it is not accessible
             int neighbors = 0;
             for (int i = -1; i < 2; i++) {
                 for (int j = -1; j < 2; j++) {
-                    if (i == j & j == 0) continue;
-                    if (get(grid, x+i, y+j)) neighbors++;
+                    if (i == j & j == 0) continue; // Center of looking is the roll itself, you are not your own neighbor
+                    if (get(grid, x+i, y+j)) neighbors++; // Increase neighbors count if any number is present in neighboring cell
                 }
             }
-            if (neighbors < 4) {
+            if (neighbors < 4) { // If less than 4 neighbors, increase total index and mark for removal
                 total++;
-                grid[y][x] = 2;
+                grid[y][x] = 2; // Grid 0 means nothing, 1 means roll, 2 means roll and remove this roll
             }
         }
     }
@@ -64,7 +65,7 @@ int countAllNeighbors(int** grid) {
 void removePaperRolls(int** grid) {
     for (int y = 0; y < GRID_SIZE; y++) {
         for (int x = 0; x < GRID_SIZE; x++) {
-            grid[y][x] %= 2;
+            grid[y][x] %= 2; // Remove rolls by self modulo, turns 2 into 0, but keeps 1
         }
     }
 }
@@ -72,11 +73,11 @@ void removePaperRolls(int** grid) {
 int main() {
     int** grid = readGrid();
     int amount = countAllNeighbors(grid);
-    int next = amount;
+    int next = amount; // Needed to start loop for the first time
     while (next) {
-        removePaperRolls(grid);
-        next = countAllNeighbors(grid);
-        amount += next;
+        removePaperRolls(grid); // Remove all rolls marked for removal
+        next = countAllNeighbors(grid); // Calculate new rolls that are accessible
+        amount += next; // Add rolls marked for remocal
     }
     
     freeMatrix(grid, GRID_SIZE);
